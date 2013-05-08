8a352f4ab97123399462d482bfb945ac887aad5c
Ahmed Ali <aja.ali@gmail.com>
2013-05-08 23:28:50 +0100

    new models and icons

--- printable/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class PrintableCV(models.Model):
-    url_hash = models.CharField(max_length=32, db_index=True)
-    cv_parameters = models.TextField()
